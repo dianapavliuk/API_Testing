3a2d1cf8c35e9bb4dbcb7699ce167377b7df617b
dianapavliuk <dianapavliuk22@gmail.com>
2023-10-28 16:29:18 +0300

    new

---
+++ src/main/java/dto/ErrorDTO.java
@@ -0,0 +1,18 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@Builder
+@ToString
+public class ErrorDTO {
+
+            int status;
+            String error;
+            String message;
+          //  "path": "string"
+}
---
+++ src/test/java/okhttp/LoginTests.java
@@ -0,0 +1,50 @@
+package okhttp;
+
+import com.google.gson.Gson;
+import dto.AuthResponseDTO;
+import dto.AutoRequestDTO;
+import dto.ErrorDTO;
+import okhttp3.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTests {
+    public static  final MediaType JSON = MediaType.get("application/json;charset=utf-8");
+    Gson gson= new Gson();
+    OkHttpClient client = new OkHttpClient();
+    String baseURL = "https://contactapp-telran-backend.herokuapp.com";
+
+    //eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwic3ViIjoibG92ZUBnbWFpbC5jb20iLCJpc3MiOiJSZWd1bGFpdCIsImV4cCI6MTY5OTA5Njk2NywiaWF0IjoxNjk4NDk2OTY3fQ._llAmj4iW2J7ACwJBNs5vUnn3t3SkDQonpAq0iEbfyw
+    @Test
+    public void loginPositive() throws IOException {
+        AutoRequestDTO requestDTO = AutoRequestDTO.builder()
+                .username("love@gmail.com")
+                .password("abC250712#")
+                .build();
+        RequestBody requestBody = RequestBody.create(gson.toJson(requestDTO),JSON);
+
+        Request request = new Request.Builder()
+                .url(baseURL+"/v1/user/login/usernamepassword")
+                .post(requestBody)
+                .build();
+        Response response = client.newCall(request).execute();
+        if(response.isSuccessful()){
+           String responseJson =  response.body().string();
+            AuthResponseDTO responseDTO = gson.fromJson(responseJson, AuthResponseDTO.class );
+
+            System.out.println("Response code is -----> " + response.code());
+            System.out.println(responseDTO.getToken());
+            Assert.assertTrue(response.isSuccessful());
+
+        } else {
+            ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+            System.out.println("Response code is -----> " + response.code());
+            System.out.println(errorDTO.getStatus()+"========"+errorDTO.getMessage()+"========"+errorDTO.getError());
+            Assert.assertFalse(response.isSuccessful());
+
+        }
+
+    }
+}
